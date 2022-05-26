c42aa5477d910af7e12eb62b8d7a53d663e79ce8
Collins Rollins <collinsrollins07@gmail.com>
2022-05-26 07:19:56 +0100

    updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
@@ -7,6 +8,8 @@ import {
   setUserData,
 } from "../redux/slices/authSlice";
 import { useSelector, useDispatch } from "react-redux";
+import Cookies from "universal-cookie";
+
 
 const Dashboard = ({ newData }) => {
   const dispatch = useDispatch();
@@ -14,8 +17,13 @@ const Dashboard = ({ newData }) => {
   const user_data = useSelector(getUserData);
   const [btn, setBtn] = useState("click here to logout");
   const navigate = useNavigate();
+  const cookies = new Cookies();
   const handleLogout = async () => {
     try {
+      cookies.remove('token');
+      dispatch(setValidate(false))
+      navigate("/login");
+      return
       setBtn("Logging out....");
       const res = await fetch(`https://auth-checkout-server.vercel.app/logout`, {
         method: "GET",
